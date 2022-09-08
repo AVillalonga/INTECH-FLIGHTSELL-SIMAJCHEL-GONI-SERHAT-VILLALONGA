419f426974ce517afb2114e6ca79d3e9230a468b
A.Villalonga <adrien.villalonga@gmail.com>
2022-09-08 14:59:44 +0200

    build: api squeleton

---
+++ api/flight.controller.js
@@ -0,0 +1,11 @@
+/**
+ * Constructeur de requete
+ */
+
+module.export = (app) => {
+
+    app.get()
+
+    app.post()
+
+}
---
+++ api/index.js
@@ -0,0 +1,12 @@
+/**
+ * CREER L'APP FASTIFY ICI
+ */
+
+import flightController from "./flight.controller.js";
+
+function main() {
+    const app = null;
+    flightController(app);
+}
+
+main();
