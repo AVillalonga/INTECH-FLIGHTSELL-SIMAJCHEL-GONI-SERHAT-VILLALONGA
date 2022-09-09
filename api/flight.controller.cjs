27b1250859b6dc462be5009494d2b7c0359b3b75
Apsum <baptiste.goni@outlook.fr>
2022-09-09 14:19:28 +0200

    First API get

--- api/flight.controller.cjs
+++ api/flight.controller.cjs
@@ -2,11 +2,21 @@
  * Constructeur de requete
  */
 
+
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+
 /**
  * Encapsulates the routes
  * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
  */
 async function flightController(fastify) {
+    // Connection to Database
+    fastify.register(require('@fastify/mysql'), {
+        connectionString: 'mysql://root:MySQL@localhost:3306/flight_sell'
+    })
+
     /**
      * Contain and synchronize all schema of flight controller
      * @param {FastifyInstance} fastify
@@ -56,6 +66,9 @@ async function flightController(fastify) {
                     },
                 },
                 handler: async (request, reply) => {
+                    fastify.mysql.query(
+                        'INSERT into users (name,mail,password) VALUES (?,?,password)', [request.body.user_name, request.body.user_mail]
+                    )
                     return { response: "Billets achetés " };
                 },
             },
@@ -65,29 +78,58 @@ async function flightController(fastify) {
                 schema: {
                     response: {
                         200: {
-                            type: "object",
-                            properties: {
-                                ref1: { type: "string" },
-                                departureName: { type: "string" },
-                                arrivalName: { type: "string" },
-                                price: { type: "string" },
-                            },
                         },
                     },
                 },
                 handler: async (request, reply) => {
-                    return {
-                        ref1: "A3456",
-                        departureName: "CDG",
-                        arrivalName: "JFK",
-                        price: "17",
-                    };
+                    fastify.mysql.query(
+                        'SELECT * from flights', 
+                    )
+                    function onResult (err, result) {
+                        reply.send(err || result)
+                      }
                 },
             },
         ];
 
         for (const route of routes) fastify.route(route);
     }
+    
+    const schemaFlight = {
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              value: { type: 'string' },
+              otherValue: { type: 'boolean' }
+            }
+          }
+        }
+      }
+      fastify.get('/flightList', function(req, reply) {
+        fastify.mysql.query(
+          'SELECT * from flights',
+          function onResult (err, result) {
+            reply.send(err || result)
+          }
+        )
+      })
+    // Test route for connection
+      fastify.get('/flightall', function(req, reply) {
+        const flights = prisma.flights.findMany()
+        const flightsContracts = []
+        /*for (const flight of flights){
+            const flightDeparture = prisma.locations.findUnique({where: {id_locations : flight.departure_id}})
+            const flightDestination = prisma.locations.findUnique({where: {id_locations : flight.destination_id}})
+            flightsContracts.push({
+                ref1: flight.ref,
+                departureName: flightDeparture,
+                arrivalName: flightDestination,
+                price: flight.price
+            });
+        }   */
+        reply.send(flights)
+      })
     
     loadSchemas(fastify);
     loadRoutes(fastify);
