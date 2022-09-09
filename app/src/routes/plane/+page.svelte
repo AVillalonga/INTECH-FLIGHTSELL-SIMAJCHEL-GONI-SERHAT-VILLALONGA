5cd8d3fe68a77ffd7a5de081c524b2a9dd93ba4c
yann <yann.minuit@yahoo.fr>
2022-09-09 10:44:29 +0200

    added logic

--- app/src/routes/plane/+page.svelte
+++ app/src/routes/plane/+page.svelte
@@ -1,24 +1,48 @@
 <script>
     import { savedFlight } from '../../store.js';
 
-    let flight;
-    
-    let name;
+    let flights = [];
+
+    let name = '';
     let age;
-    let email;
+    let email = '';
 
 	savedFlight.subscribe(value => {
-		flight = value;
+        if(Object.prototype.toString.call(value) === '[object Array]') {
+            flights = [...value];
+        }
 	});
+
+    function sendInfos() {
+        alert(`data is ${name} ${age} ${email}`)
+    }
 
 </script>
 
-<h1>Plane is {flight ? flight.text : 'XXX'}</h1>
+<div class="container">
+
+<h6>Plane are : </h6>
+<ul class="list-group">
+	{#each flights as { id, name}, i}
+		<li class="list-group-item">
+            {name}
+        </li>
+	{/each}
+</ul>
 <p>Fill your informations</p>
 
-<input value={name}>
-<input type="date" value={age}>
-<input type="email" value={email}>
+<form on:submit|preventDefault={sendInfos}>
+    <input type="text" bind:value={name}>
+    <input type="date" bind:value={age}>
+    <input type="email" bind:value={email}>
+    <button type=submit class="btn btn-primary">
+		Validate my informations
+	</button>
+</form>
+</div>
 
-
-
+<style>
+    .container {
+        text-align: center;
+    }
+</style>
