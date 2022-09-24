d3d2476466f3c485b2a477f4cdf2b1e8f79f0ff0
A.Villalonga <adrien.villalonga@gmail.com>
2022-09-24 12:07:42 +0200

    remove old api

--- api/src/router.js
+++
@@ -1,9 +0,0 @@
-import flight from "./controller/flight.controller.js";
-
-/**
- * Api Router
- * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
- */
-export default async function(fastify) {
-    await flight(fastify);
-};
