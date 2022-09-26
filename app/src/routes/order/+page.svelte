737c8eaa81818bb85c04cf68a9e22317f8341347
Yann Simajchel <yann.minuit@yahoo.fr>
2022-09-26 14:16:23 +0200

    forgot :frog:

--- app/src/routes/order/+page.svelte
+++ app/src/routes/order/+page.svelte
@@ -2,12 +2,35 @@
     <h2>Thanks for traveling with us</h2>
     Here is a recap of your order
 
-    <div class="list-group">
+    {#if order}
+        <div class="list-group">
+            {#each flights as flight}
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                    <h6 class="mb-1">
+                        {flight.departure} - {flight.destination}
+                    </h6>
+                    <small class="text-muted">{parseFloat(flight.price) * (flight.hasOwnProperty('devise') ? parseFloat(flight.devise.value) : 1)} {(flight.hasOwnProperty('devise') ? flight.devise.name : "€")}</small>
+                </div>
+                {#if flight.options.filter(opt => opt.checked).length > 0}
+                    <small>With the following options</small>
+                {/if}
+                    {#each flight.options as option}
+                        {#if option.checked}
+                            <div class="d-flex w-100 justify-content-between">
+                                <small class="text-muted">{option.name}</small>
+                                <small class="text-muted">{option.value} { option.isPercent === 0 ? 'euros' : '%' }</small>
+                            </div>
+                        {/if}
+                    {/each}
+            </a>
+        {/each}
+        
+        
+        
+        </div>
+    {/if}
 
-       
-       
-    
-      </div>
 
 
 
--- app/src/routes/plane/+page.svelte
+++ app/src/routes/plane/+page.svelte
@@ -39,26 +39,26 @@
     <h6>Plane are : </h6>
     <ul class="list-group">
         {#each flights as flight}
-        <a href="#" class="list-group-item list-group-item-action">
-            <div class="d-flex w-100 justify-content-between">
-                <h6 class="mb-1">
-                    {flight.departure} - {flight.destination}
-                </h6>
-                <small class="text-muted">{parseFloat(flight.price) * (flight.hasOwnProperty('devise') ? parseFloat(flight.devise.value) : 1)} {(flight.hasOwnProperty('devise') ? flight.devise.name : "€")}</small>
-            </div>
-            {#if flight.options.filter(opt => opt.checked).length > 0}
-                <small>With the following options</small>
-            {/if}
-                {#each flight.options as option}
-                    {#if option.checked}
-                        <div class="d-flex w-100 justify-content-between">
-                            <small class="text-muted">{option.name}</small>
-                            <small class="text-muted">{option.value} { option.isPercent === 0 ? 'euros' : '%' }</small>
-                        </div>
-                    {/if}
-                {/each}
-        </a>
-    {/each}
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                    <h6 class="mb-1">
+                        {flight.departure} - {flight.destination}
+                    </h6>
+                    <small class="text-muted">{parseFloat(flight.price) * (flight.hasOwnProperty('devise') ? parseFloat(flight.devise.value) : 1)} {(flight.hasOwnProperty('devise') ? flight.devise.name : "€")}</small>
+                </div>
+                {#if flight.options.filter(opt => opt.checked).length > 0}
+                    <small>With the following options</small>
+                {/if}
+                    {#each flight.options as option}
+                        {#if option.checked}
+                            <div class="d-flex w-100 justify-content-between">
+                                <small class="text-muted">{option.name}</small>
+                                <small class="text-muted">{option.value} { option.isPercent === 0 ? 'euros' : '%' }</small>
+                            </div>
+                        {/if}
+                    {/each}
+            </a>
+        {/each}
     {#if flights.length != 0 && totalPrice}
         <h5 class="list-group-item">Total is 
             {(parseFloat(totalPrice) * (flights[0].hasOwnProperty('devise') ? parseFloat(flights[0].devise.value) : 1)).toFixed(2)} {(flights[0].hasOwnProperty('devise') ? flights[0].devise.name : "€")}
