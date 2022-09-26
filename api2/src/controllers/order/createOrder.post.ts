c532f1006ec52dac9f7dcd4c059d96b694e51bc2
A.Villalonga <adrien.villalonga@gmail.com>
2022-09-26 16:07:38 +0200

    fix createorder

--- api2/src/controllers/order/createOrder.post.ts
+++ api2/src/controllers/order/createOrder.post.ts
@@ -1,6 +1,6 @@
 import mailService from "../../services/mail.service.js";
 import orderService from "../../services/order.service.js";
-import { parseOrderToDTO } from "../../dal/dto/order.dto.js";
+import { parseOrderIdToDTO } from "../../dal/dto/order.dto.js";
 
 export const orderSchema = {
     response: {
@@ -18,9 +18,36 @@ export async function createOrder(req: any, rep: any) {
         req.body = JSON.parse(req.body);
     }
     const order = await orderService.createOrder(req.body);
-    rep.send(parseOrderToDTO(order));
+
+    // await PrismaService.order.findUnique({
+    //     select: {
+    //         ticket: {
+    //             select: {
+    //                 id: true,
+    //                 flight_flightToticket: {
+    //                     select: {
+    //                         direction_directionToflight: {
+    //                             select: {
+    //                                 location_direction_departureTolocation: {
+    //                                     select: {name: true}
+    //                                 }
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     },
+    //     where: { id: order.id }
+    // });
+
+    rep.send(parseOrderIdToDTO(order));
     //Send mail after req.send to not block the UI
-    await mailService.sendMail(order.user_orderTouser.mail, 
-        `Votre commande à été confirmé (#${order.id})`, 
-        `Merci d\'avoir commander chez nous ${order.ticket.length} tickets.\n ${order.ticket.map((t) => t.price).join("€ ")}`);
+    await mailService.sendMail(
+        order.user_orderTouser.mail,
+        `Votre commande à été confirmé (#${order.id})`,
+        `Merci d\'avoir commander chez nous ${
+            order.ticket.length
+        } tickets.\n ${order.ticket.map((t) => t.price).join("€ ")}`
+    );
 }
